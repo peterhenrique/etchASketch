29de68885e877d7883fbff6711de8773b9ae4bb9
peterhenrique <pedrohsdeoliveira27@gmail.com>
2022-03-20 13:49:14 -0300

    index.html

--- javascript/script.js
+++ javascript/script.js
@@ -1,27 +1,56 @@
 
 //grid
-const container = document.querySelector('#container');
+const container = document.getElementById('container');
 
-function gridmaker (a) {
-    for (i = 0; i < (a*a); i++ ) {
+console.log(container)
+
+
+function gridmaker (a) {  
+
+    for (i = 0; i < (a*a); i++ ) {       
     const grid = document.createElement('grid');
     grid.classList.toggle('grid');
     container.appendChild(grid);
+    grid.style.height = `${(640/a) -2 }px`;
+    grid.style.width = `${(640/a) -2}px`;
+    grid.style.maxHeight = `${(640/a) }px`;
+    grid.style.maxWidth = `${(640/a)}px`;
     grid.addEventListener('mouseover', () => {
-        grid.style.backgroundColor = 'black';
-    })
+        const randomcolor = Math.floor(Math.random()*16777215).toString(16);
+      
+    grid.style.backgroundColor = '#' + randomcolor
+    })}
 
 }
 
-    }
+    
+
 
 gridmaker(16);
 
 
+function removeAllChildNodes (container){
+    while (container.firstChild){
+        container.removeChild(container.firstChild)
+    }}
+
+
 const botao = document.querySelector('#btn');
 botao.addEventListener('click', () => {
-gridmaker(16)
-})
+
+    let question = prompt ('How many?')
+
+    if (question >= 100) {
+        return alert ("Please select a number smaller than 100 ")
+    } else {    
+    removeAllChildNodes(container);
+
+    
+
+    gridmaker(question)
+    
+    }}
+)
 
 //apagar function gridmaker
 
